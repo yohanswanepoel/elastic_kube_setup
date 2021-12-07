b26f9b7d4004d18d2a1b17ddbf617f9fe4252082
Johan Swanepoel <johanswanepoel@Johans-MacBook-Pro.local>
2021-12-07 11:16:50 +1100

    Made some changes to work with minikube crio - workaround until docker file fixed

--- setup-agents.py
+++ setup-agents.py
@@ -5,7 +5,7 @@ import platform
 
 # Installing the state-metrics-service
 # this goes into the kube-system namespace
-os.system("git clone https://github.com/kubernetes/kube-state-metrics.git kube-state-metrics"
+os.system("git clone https://github.com/kubernetes/kube-state-metrics.git kube-state-metrics")
 os.system("kubectl create -f kube-state-metrics/examples/standard")
 os.system("rm -rf kube-state-metrics")
 
